cfdab0f7618fc0209d4e2ae8ecffa47b50df0891
Narumikazuchi <euwkamigoroshi@yahoo.de>
2021-09-17 21:47:29 +0200

    Updated.

--- Narumikazuchi.Hiragana_And_Katakana_SelfTester/obj/Release/net5.0-windows/Main/MainWindow.g.cs
+++ Narumikazuchi.Hiragana_And_Katakana_SelfTester/obj/Release/net5.0-windows/Main/MainWindow.g.cs
@@ -175,7 +175,7 @@ namespace Narumikazuchi.Hiragana_And_Katakana_SelfTester {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.5.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.6.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -191,7 +191,7 @@ namespace Narumikazuchi.Hiragana_And_Katakana_SelfTester {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.5.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.6.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
